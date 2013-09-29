3914ae2c46e17ab2730b8c561165f6c0056dc915
Narender Yadav <narender1412@gmail.com>
2013-09-29 09:03:37 +0530

    Update download.py

--- Network/download.py
+++ Network/download.py
@@ -6,9 +6,6 @@ save = sys.argv[2]    # Save As in Welcome.php
 print url
 doc = urlopen(url)
 html=doc.read( )
-print "accessed"
-print "why"
-#f=open("http://localhost/Network/storage/nirvana2.html","w")
 f = open("/var/www/Network/"+save+"","w")
 
 f.write(html)
